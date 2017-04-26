9a17a5db5176687602da2956ea9a6d4b5a4faa18
Salvador Orozco Villalever <salvador@orozco.in@>
2017-04-26 17:09:14 -0500

    new common errors added

---
+++ medical-database/resources/views/docProfile.blade.php
@@ -0,0 +1,71 @@
+<html>
+
+	<head>
+		<title>B&uacute;squeda doctor</title>
+		<link rel="stylesheet" href="/css/docStyles.css">
+	</head>
+
+	<body>
+		<br>
+		<div class="titleBox">
+			<br>
+			<h1>Dr(a). {{ $doctor->first_name }} {{ $doctor->last_name }}</h1>
+			<center><img src="estrellas.png" width="250"></center>
+			<br>
+			<br>
+		</div>
+
+		<div class="boxed">
+			<br>
+			<table style="width:50%">
+				<tr>
+					<td class="titleTable">Nombre</td>
+					<td class="infoTable">{{ $doctor->first_name }}</td>
+				</tr>
+				<tr>
+					<td class="titleTable">Apellido</td>
+					<td class="infoTable">{{ $doctor->last_name }}</td>
+				</tr>
+				<tr>
+					<td class="titleTable">C&eacute;dula profesional</td>
+					<td class="infoTable">{{ $doctor->medical_id }}</td>
+				</tr>
+				<tr>
+					<td class="titleTable">Especialidad</td>
+					<td class="infoTable">{{ $doctor->specialty }}</td>
+				</tr>
+				<tr>
+					<td class="titleTable">Costo por consulta</td>
+					<td class="infoTable">{{ $doctor->consultation_cost }}</td>
+				</tr>
+			</table>
+
+			<br>
+			<br>
+			<br>
+			<br>
+			<h2>Rese&ntilde;as</h2>
+			<table style="width: 80%">
+				<tr>
+					<td class="patientTable">Salvador Orozco</td>
+					<td class="patientTable">Hace 4 horas</td>
+			</table>
+			<div class="reviewDiv">
+				Me pareci&oacute; una consulta excelente, a pesar de que el doctor lleg&oacute; tarde.
+			</div>
+			<br>
+			<br>
+			<table style="width: 80%">
+				<tr>
+					<td class="patientTable">Francisco Mendoza</td>
+					<td class="patientTable">Hace 5 horas</td>
+			</table>
+			<div class="reviewDiv">
+				No me gust&oacute; el trato que tuvo conmigo, ya que fue irrespetuoso y abusivo.
+			</div>
+
+		</div>
+
+		<center><button class="button1">Iniciar sesi&oacute;n</button></center>
+	</body>
+</html>
--- medical-database/routes/web.php
+++ medical-database/routes/web.php
@@ -12,5 +12,14 @@
 */
 
 Route::get('/', function () {
+    
     return view('all_doctors');
+});
+
+// Route::get('/search_doctor/{doctor_id}', function($doctor_id)){
+
+Route::get('/search_doctor/1', function() {
+	
+	$doctor = App\Doctor::where('id', 1)->get();
+	return view('docProfile', $doctor);
 });
