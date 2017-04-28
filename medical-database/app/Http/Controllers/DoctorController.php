90baeccb6ec16c40c32c3335d362bfcc10774bb7
Salvador Orozco Villalever <salvador@orozco.in@>
2017-04-28 18:10:59 -0500

    fixed guest access

--- medical-database/app/Http/Controllers/DoctorController.php
+++ medical-database/app/Http/Controllers/DoctorController.php
@@ -8,7 +8,17 @@ use App\Specialty;
 use Illuminate\Http\Request;
 
 class DoctorController extends Controller
-{
+{		
+	/**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
     public function show($doctor_id){
 
 		$doctor = Doctor::where('id', $doctor_id)->firstOrFail(); 
