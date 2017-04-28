8f3f4f591d221a63454abed7f746bab64708bbe0
Salvador Orozco Villalever <salvador@orozco.in@>
2017-04-28 13:49:20 -0500

    HospitalController was added

--- medical-database/routes/web.php
+++ medical-database/routes/web.php
@@ -31,31 +31,7 @@ Route::get('/show_doctor/', function() {
 });
 
 // Route::get('/show_hospital/{hospital_id}', function($hospital_id) {
-Route::get('/show_hospital/', function() {
-
-	$hospital_id = 2;
-
-	$hospital = App\Hospital::where('id', $hospital_id)->first(); 
-
-	$total_doctors = DB::table('hospitals')
-		->select('hospitals.address_id', 'hospitals.id')
-		->where('hospitals.id', '=', $hospital_id)
-		->join('offices', 'hospitals.id', '=', 'offices.hospital_id')
-		->join('doctors_offices', 'offices.id', '=', 'doctors_offices.id')
-		->join('doctors', 'doctors.id', '=', 'doctors_offices.id')
-		->join('addresses', 'addresses.id', '=', 'offices.address_id')
-		->select('doctors.first_name', 'doctors.last_name', 'addresses.id')
-		->get();
-
-	$address = DB::table('hospitals')
-		->select('hospitals.address_id', 'hospitals.id')
-		->where('hospitals.id', '=', $hospital_id)
-		->join('addresses', 'hospitals.address_id', '=', 'addresses.id')
-		->select('addresses.street', 'addresses.number', 'addresses.suburb')
-		->first();
-
-	return view('hospProfile', ['hospital' => $hospital, 'total_doctors' => $total_doctors, 'address' => $address]);
-});
+Route::get('/show_hospital/{hospital_id}', "HospitalController@show");
 
 Route::get('/search_hospital/', function() {
 
