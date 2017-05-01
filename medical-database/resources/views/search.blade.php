953b3d7c18a4aef6ee6e72868be8cb25c77f86a2
Aranzza Abascal <arabascalf@gmail.com>
2017-05-01 13:13:44 -0500

    New search view

--- medical-database/resources/views/search.blade.php
+++ medical-database/resources/views/search.blade.php
@@ -2,134 +2,40 @@
 
 @section('content')
 
-<html>
-    
-    <head>
-        <title>Pagina de inicio</title>
 
-        <script>
-            
-            function habilitarOpt1() {
+<div class="box1">
+    <h1>Base de datos de servicios m&eacute;dicos</h1>
+    <br>
+    <h2><center>TE AYUDAMOS A ENCONTRAR DOCTORES Y HOSPITALES.
+    <br>BUSCA CON FILTROS Y SELECCIONA LA MEJOR OPCI&Oacute;N
+    <br>QUE SE ADAPTE A TUS NECESIDADES Y REQUISITOS
+    </h2></center>
 
-                document.getElementById("opt2").style.visibility = "hidden"
-                document.getElementById("opt1").style.visibility = "visible"
-            }
+    <br>
+    <br>
+    <br>
+    <center>
+    <FORM METHOD="LINK" ACTION="loginOpt"> <INPUT TYPE="submit" VALUE="Iniciar sesi&oacute;n"> </FORM>
 
-            function habilitarOpt2() {
+    <FORM METHOD="LINK" ACTION="suscribeOpt"> <INPUT TYPE="submit" VALUE="Suscribir"> </FORM>
+    </center>
+    <br>
+    <br>
+    <br>
+    <br>
+</div>
 
-                document.getElementById("opt1").style.visibility = "hidden"
-                document.getElementById("opt2").style.visibility = "visible"
-            }
+<div class="box2">
+    Realizar una b&uacute;squeda
+</div>
 
-        </script>
+<div class="box3">
 
-    </head>
-
-    <body>
-        <br>
-        <br>
-
-        <div class="boxed">
-            <h1><center>Base de datos de servicios m&eacute;dicos</h1></center>
-
-            <br>
-
-            <h2><center>TE AYUDAMOS A ENCONTRAR DOCTORES Y HOSPITALES.
-            <br>BUSCA CON FILTROS Y SELECCIONA LA MEJOR OPCI&Oacute;N
-            <br>QUE SE ADAPTE A TUS NECESIDADES Y REQUISITOS
-            </h2></center>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-
-            <!-- <button class="button1">Iniciar sesi&oacute;n</button> -->
-            <FORM METHOD="LINK" ACTION="loginOpt"> <INPUT TYPE="submit" class="button1" VALUE="Iniciar sesi&oacute;n"> </FORM>
-
-            <!-- <button class="button2">Suscribir</button> -->
-            <FORM METHOD="LINK" ACTION="suscribeOpt"> <INPUT TYPE="submit" class="button2" VALUE="Suscribir"> </FORM>
-
-        </div>
-
-        <br>
-
-        <br>
-            <center><h2>Buscar por:</h2></center>
-
-            <center>
-            {!! Form::open(array('action' => 'SearchController@default')) !!}
-                
-                {{ Form::label('check', 'Doctor', array('id' => '1', 'class' => '')) }}
-                {{ Form::radio('check', '1', ['class' => 'regular-checkbox']) }}
-
-
-            <!-- {!! Form::open(array('action' => 'SearchController@default')) !!}
-            
-                {{ Form::radio('check', '1', ['class' => 'regular-checkbox']) }}<br>
-                {{ Form::radio('check', '2', ['class' => 'regular-checkbox']) }}
->>>>>>> Stashed changes
-
-                <br>
-                <br>
-                {{ Form::label('check', 'Hospital', array('id' => '2', 'class' => '')) }}
-                {{ Form::radio('check', '2', ['class' => 'regular-checkbox']) }}
-                <br>
-                <br> 
-
-                @if('check' == '1')
-                     {{ Form::select('option', $search_types[1]) }}  
-                @elseif('check' == '2')
-                    {{ Form::select('option', $search_types[2]) }}  
-                @endif
-
-                {{ Form::text('search_text','Ingrese su búsqueda',array('id'=>'3','class'=>'')) }}
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-
-                {{ Form::submit('Enviar!') }}
-                <br>
-                <br>            
-
-<<<<<<< Updated upstream
-            {{ Form::close() }}
-            </center>
-            
-            <!-- <div id="opt1">
-=======
-            {{ Form::close() }} -->
-            
-            <div id="opt1" class="opt1">
-
-
-        <br>
-        <br>
-        <br>
-       <!--  <center>
-        <input type="text" id="search" name="search" placeholder="Ingresa tu b&uacute;squeda">
-        </center> -->
-        <!-- <button class="button3">Buscar</button> -->
-        <!-- <FORM METHOD="LINK" ACTION="searching"> <INPUT TYPE="submit" class="button3" VALUE="Buscar"> </FORM> -->
-
-        <br>
-        <br>
-        <br>
-         <br>
-        <br>
-        <br>
-         <br>
-        <br>
-        <br>
-
-    </body>
-
-</html>
+    <br>
+    <FORM METHOD="LINK" ACTION="loginOpt"> <INPUT TYPE="submit" class="button2" VALUE="Buscar doctor"> </FORM>
+    <FORM METHOD="LINK" ACTION="loginOpt"> <INPUT TYPE="submit" class="button3" VALUE="Buscar hospital"> </FORM>
+    <br>
+    <br>
+    <br>
+    <br>
+</div>
