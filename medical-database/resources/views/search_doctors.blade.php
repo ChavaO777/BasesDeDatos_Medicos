1481ee25b37853eac2f2b98ad3d688629c9dc084
guillegard <guillegard95@gmail.com>
2017-05-01 14:46:38 -0500

    Busca Doctores

--- medical-database/resources/views/search_doctors.blade.php
+++ medical-database/resources/views/search_doctors.blade.php
@@ -5,23 +5,7 @@
 <html>
     
     <head>
-        <title>Pagina de inicio</title>
-
-        <script>
-            
-            function habilitarOpt1() {
-
-                document.getElementById("opt2").style.visibility = "hidden"
-                document.getElementById("opt1").style.visibility = "visible"
-            }
-
-            function habilitarOpt2() {
-
-                document.getElementById("opt1").style.visibility = "hidden"
-                document.getElementById("opt2").style.visibility = "visible"
-            }
-
-        </script>
+        <title>Busca Doctores</title>
 
     </head>
 
@@ -30,30 +14,9 @@
         <br>
 
         <div class="boxed">
-            <h1><center>Base de datos de servicios m&eacute;dicos</h1></center>
+            <h1><center>Busca doctores</h1></center>
 
             <br>
-
-            <h2><center>TE AYUDAMOS A ENCONTRAR DOCTORES Y HOSPITALES.
-            <br>BUSCA CON FILTROS Y SELECCIONA LA MEJOR OPCI&Oacute;N
-            <br>QUE SE ADAPTE A TUS NECESIDADES Y REQUISITOS
-            </h2></center>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-
-            <!-- <button class="button1">Iniciar sesi&oacute;n</button> -->
-            <FORM METHOD="LINK" ACTION="loginOpt"> <INPUT TYPE="submit" class="button1" VALUE="Iniciar sesi&oacute;n"> </FORM>
-
-            <!-- <button class="button2">Suscribir</button> -->
-            <FORM METHOD="LINK" ACTION="suscribeOpt"> <INPUT TYPE="submit" class="button2" VALUE="Suscribir"> </FORM>
 
         </div>
 
@@ -64,21 +27,16 @@
             <center>
             {!! Form::open(array('action' => 'SearchController@default')) !!}
                 
-                {{ Form::label('check', 'Doctor', array('id' => '1', 'class' => '')) }}
-                {{ Form::radio('check', '1', ['class' => 'regular-checkbox']) }}
+                
 
+               
+                {{ Form::select('option', array('first_name' => 'Nombre',
+                                                'last_name' => 'Apellido',
+                                                'specialty' => 'Especialidad',
+                                                'medical_id' => 'Cédula')) }}  
                 <br>
                 <br>
-                {{ Form::label('check', 'Hospital', array('id' => '2', 'class' => '')) }}
-                {{ Form::radio('check', '2', ['class' => 'regular-checkbox']) }}
-                <br>
-                <br> 
-
-                @if('check' == '1')
-                     {{ Form::select('option', $search_types[1]) }}  
-                @elseif('check' == '2')
-                    {{ Form::select('option', $search_types[2]) }}  
-                @endif
+               
 
                 {{ Form::text('search_text','Ingrese su búsqueda',array('id'=>'3','class'=>'')) }}
                 <br>
--- medical-database/resources/views/search_results.blade.php
+++ medical-database/resources/views/search_results.blade.php
@@ -1,8 +1,7 @@
 @extends('layouts.app')
 
 <?php
-$var1 = $_POST['check'];
-echo $var1;
+
 $var2 = $_POST['search_text'];
 echo $var2;
 $var3 = $_POST['option'];
