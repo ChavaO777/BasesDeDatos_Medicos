f07ba7af07a01aad1aee0f35fa5fdd052ce0cc1d
guillegard <guillegard95@gmail.com>
2017-05-01 19:33:22 -0500

    Search complete

--- medical-database/app/Http/Controllers/SearchController.php
+++ medical-database/app/Http/Controllers/SearchController.php
@@ -12,8 +12,18 @@ class SearchController extends Controller
 		$selectedCriteria = $_POST['option'];
 		$lookFor = $_POST['search_text'];
 
-		$total_doctors = DB::table('doctors')->
+		if($selectedCriteria != 'specialty'){
+			$total_doctors = DB::table('doctors')->
 						where($selectedCriteria, '=', $lookFor)->get();
+		}else{
+			$specialty = DB::table('specialties')->
+							where('name', '=', $lookFor)->
+							select('id')->first();
+
+			$total_doctors = DB::table('doctors')->
+							where('doctors.specialty_id', '=', $specialty->id)->get();
+		}
+		
 
     	return view('searchResults', ['total_doctors' => $total_doctors, 'flag' => 1]);
     }
@@ -22,10 +32,21 @@ class SearchController extends Controller
 
     	$selectedCriteria = $_POST['option'];
 		$lookFor = $_POST['search_text'];
-    	
-		$total_hospitals = DB::table('hospitals')->
-						where($selectedCriteria, '=', $lookFor)->get();
 
+		if($selectedCriteria == 'name'){
+			$total_hospitals = DB::table('hospitals')->
+							where($selectedCriteria, '=', $lookFor)->get();
+		}else{
+			$city = DB::table('cities')->
+							where('name', '=', $lookFor)->
+							select('id')->first();
+
+			$total_hospitals = DB::table('hospitals')->
+							where('hospitals.id', '=', $city->id)->get();
+		}
+    	
+			
+		//dd($city_id);
     	return view('searchResults', ['total_hospitals' => $total_hospitals, 'flag' => 2]);
     }
 }
