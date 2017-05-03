f67b5a2db707e4fbbe3e4176336d64d0027f9fe6
Aranzza Abascal <arabascalf@gmail.com>
2017-05-02 20:54:54 -0500

    Better design for searching results views

--- medical-database/resources/views/searchResults.blade.php
+++ medical-database/resources/views/searchResults.blade.php
@@ -10,6 +10,7 @@
 <div class="box2">Resultados de tu b&uacute;squeda</div>
 
 <div class="box3">
+	<br>
 	<center>
 
 
@@ -22,12 +23,12 @@
 
 				<p class="hide">{{ Form::number('doctor_id', $doctor -> id)}}</p>
 
-				<p> {{$doctor -> first_name}} {{$doctor -> last_name}}
+				<p><h2> {{$doctor -> first_name}} {{$doctor -> last_name}}
 				{{ Form::submit('Ver perfil')}}
-				</p>
+				</p></h2>
 
 			@empty
-				<p>No se encontro ningun resultado</p>
+				<p><h3>No se encontro ningun resultado</h3></p>
 
 			@endforelse
 
@@ -41,12 +42,12 @@
 
 				<p class="hide">{{ Form::number('hospital_id', $hospital -> id)}}</p>
 
-				<p> {{$hospital -> name}}
+				<p><h2> {{$hospital -> name}}
 				{{ Form::submit('Ver perfil')}}
-				</p>
+				</p></h2>
 			
 			@empty
-				<p>No se encontro ningun resultado</p>
+				<p><h3>No se encontro ningun resultado</h3></p>
 
 			@endforelse
 
